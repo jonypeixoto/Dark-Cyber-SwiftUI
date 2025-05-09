5f96811d77ddaac63c873d4c96166a8402870aef
Jonathan P. Peixoto 喬尼·佩肖托 <jonypeixotooriginal@gmail.com>
2025-05-09 10:56:16 -0300

    Add files via upload

---
+++ Sources/Dark Cyber/Dark Cyber.swift
@@ -0,0 +1,26 @@
+
+import SwiftUI
+import UIKit
+
+public extension Color {
+    static var darkCyberBackground = Color(UIColor(named: "background", in: .module, compatibleWith: nil)!)
+    static let darkCyberCodeBlocks = Color(UIColor(named: "codeBlocks", in: .module, compatibleWith: nil)!)
+    static let darkCyberCurrentLine = Color(UIColor(named: "currentLine", in: .module, compatibleWith: nil)!)
+    static let darkCyberWordHighlight = Color(UIColor(named: "wordHighlight", in: .module, compatibleWith: nil)!)
+    static let darkCyberBorders = Color(UIColor(named: "borders", in: .module, compatibleWith: nil)!)
+    static let darkCyberPunctuation = Color(UIColor(named: "punctuation", in: .module, compatibleWith: nil)!)
+    static let darkCyberErrors = Color(UIColor(named: "errors", in: .module, compatibleWith: nil)!)
+    static let darkCyberAccent = Color(UIColor(named: "accent", in: .module, compatibleWith: nil)!)
+    static let darkCyberForeground = Color(UIColor(named: "foreground", in: .module, compatibleWith: nil)!)
+    static let darkCyberHeading = Color(UIColor(named: "heading", in: .module, compatibleWith: nil)!)
+    static let darkCyberTags = Color(UIColor(named: "tags", in: .module, compatibleWith: nil)!)
+    static let darkCyberStrings = Color(UIColor(named: "strings", in: .module, compatibleWith: nil)!)
+    static let darkCyberClasses = Color(UIColor(named: "classes", in: .module, compatibleWith: nil)!)
+    static let darkCyberVariables = Color(UIColor(named: "variables", in: .module, compatibleWith: nil)!)
+    static let darkCyberURLs = Color(UIColor(named: "urls", in: .module, compatibleWith: nil)!)
+    static let darkCyberNumbers = Color(UIColor(named: "numbers", in: .module, compatibleWith: nil)!)
+    static let darkCyberComments = Color(UIColor(named: "comments", in: .module, compatibleWith: nil)!)
+    static let darkCyberPreprocessor = Color(UIColor(named: "preprocessor", in: .module, compatibleWith: nil)!)
+    static let darkCyberSeparator = Color(UIColor(named: "separator", in: .module, compatibleWith: nil)!)
+    static let darkCyberFunctions = Color(UIColor(named: "functions", in: .module, compatibleWith: nil)!)
+}
